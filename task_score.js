df6a67ceabc74fe4e91be890ccd1e8bef9507eb6
[mari.shelukha@gmail.com] <maria.shelukha@nimbusweb.me>
2023-10-19 23:19:12 +0300

    adde new task_score file

---
+++ task_score.js
@@ -0,0 +1,6 @@
+let introgitScore = 100;
+let gitgitScore = 150;
+let jsgitScore = 350;
+let averageScore = (introgitScore+gitgitScore+jsgitScore)/3;
+let studentName = "Maria Shelukha";
+console.log('Средний балл по всем модулям '+ averageScore, studentName);
